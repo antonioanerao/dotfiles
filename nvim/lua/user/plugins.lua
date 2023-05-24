3026397cba82fbd4d4f4a8d5e083531735254d09
Antonio Anerão <anerao.junior@gmail.com>
2023-05-24 01:32:34 -0500

    Adicionado plugin para terminal

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -174,6 +174,17 @@ use({
   end,
 })
 
+--- Floating terminal.
+use({
+  'voldikss/vim-floaterm',
+  config = function()
+    vim.g.floaterm_width = 0.6
+    vim.g.floaterm_height = 0.6
+    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+  end
+})
+
 if packer_bootstrap then
     require('packer').sync()
 end
