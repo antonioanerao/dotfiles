7e21962d3babba3379b6ecb572adaf7457133470
Antonio Anerão <anerao.junior@gmail.com>
2023-05-26 07:07:19 -0500

    Adicionado phpactor; Adicionado snippets

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -230,6 +230,18 @@ use({
   end,
 })
 
+-- PHP Refactoring Tools
+use({
+  'phpactor/phpactor',
+  ft = 'php',
+  run = 'composer install --no-dev --optimize-autoloader',
+  config = function()
+    vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
+    vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
+  end,
+})
+
+
 if packer_bootstrap then
     require('packer').sync()
 end
