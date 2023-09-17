356b7bdb06ff7fc196d1b74020d4e2ae02b5dcb8
Antonio Anerão <anerao.junior@gmail.com>
2023-09-17 10:39:38 -0500

    Removido packer compliled e adicionado ao gitignore; Alterado arquivo de onde vem as configs do tema

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -42,27 +42,9 @@ use({
 use({
   'jessarcher/onedark.nvim',
   config = function()
-    vim.cmd('colorscheme onedark')
-
-    vim.api.nvim_set_hl(0, 'FloatBorder', {
-      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-    })
-
-     -- Make the cursor line background invisible
-    vim.api.nvim_set_hl(0, 'CursorLineBg', {
-      fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-      bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-    })
-
-    vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
-
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+    require('user/themes')
   end,
 })
-
--- Dracula theme.
-use('dracula/vim')
 
 -- Packer can manage itself
 use 'wbthomason/packer.nvim'
--- nvim/lua/user/themes.lua
+++ nvim/lua/user/themes.lua
@@ -1,15 +1,16 @@
--- One Dark theme.
-use({
-  'jessarcher/onedark.nvim',
-  config = function()
-    vim.cmd('colorscheme onedark')
+vim.cmd('colorscheme onedark')
 
-    vim.api.nvim_set_hl(0, 'FloatBorder', {
-      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-    })
-  end,
+vim.api.nvim_set_hl(0, 'FloatBorder', {
+  bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+  fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
 })
 
--- Dracula theme.
-use('dracula/vim')
+ -- Make the cursor line background invisible
+vim.api.nvim_set_hl(0, 'CursorLineBg', {
+  fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+  bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+})
+
+vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+
+vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
