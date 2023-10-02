077200640368bc7cb2ec41e4bbfef26da6ac1406
Antonio Anerão <anerao.junior@gmail.com>
2023-10-01 22:56:38 -0500

    Add novos atalhos para phpactor e splitjoin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -110,6 +110,8 @@ use({
     vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
     vim.g.splitjoin_trailing_comma = 1
     vim.g.splitjoin_php_method_chain_full = 1
+    vim.keymap.set('n', '<F3>', ':SplitjoinSplit<CR>')
+    vim.keymap.set('n', '<F4>', ':SplitjoinJoin<CR>')
   end,
 })
 
@@ -249,6 +251,7 @@ use({
   config = function()
     vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
     vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
+    vim.keymap.set('n', '<F2>', ':PhpactorFindReferences<CR>')
   end,
 })
 
