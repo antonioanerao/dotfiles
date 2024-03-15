bb019eebf0032ecb7078e1cfe44a52e3977f5331
Antonio Anerão <anerao.junior@gmail.com>
2024-03-15 09:49:07 -0500

    Ajuste cor do FloatTerm no telescope

--- nvim/lua/user/themes.lua
+++ nvim/lua/user/themes.lua
@@ -1,9 +1,7 @@
 vim.cmd('colorscheme onedark')
 
-vim.api.nvim_set_hl(0, 'FloatBorder', {
-  bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-  fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-})
+vim.api.nvim_set_hl(0, 'FloatBorder', {bg='#3B4252', fg='#5E81AC'})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#3B4252'})
 
  -- Make the cursor line background invisible
 vim.api.nvim_set_hl(0, 'CursorLineBg', {
